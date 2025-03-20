8445e0ae4ccffce09c0cc8f7a7752964fe7c75c8
Angus <angus.baker2468@gmail.com>
2025-03-20 11:31:08 +0000

    fixed?

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,9 @@ export default function Page() {
   const messages = useQuery(api.chat.getMessages);
   const sendMessage = useMutation(api.chat.sendMessage);
   const [newMessageText, setNewMessageText] = useState("");
-  const [name, setName] = useState(`${localStorage.getItem("name") || ""}`);
+  const [name, setName] = useState(
+    `${localStorage.getItem("name") || "Guest"}`
+  );
 
   useEffect(() => {
     setTimeout(() => {
